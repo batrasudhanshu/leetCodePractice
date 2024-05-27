14991bea91ac3537bd1582fb06bf537f52a0eb0d
sudhanshu1998 <sudhanshu.batra@solulever.com>
2024-05-27 10:53:08 +0530

    palindrome solution

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 class Solution {
     /**
-     * @param {number[]} nums
-     * @return {number[]}
+     * @param {string} s
+     * @return {boolean}
      */
-    productExceptSelf(nums) {
-        
-    const n = nums.length;
-    const output = new Array(n).fill(1);
-    
-    // Compute left products and accumulate in output array
-    let leftProduct = 1;
-    for (let i = 0; i < n; i++) {
-        output[i] *= leftProduct;  // Equivalent to: output[i] = output[i] * leftProduct;
-        leftProduct *= nums[i];    // Update leftProduct for the next iteration
-    }
-    
-    // Compute right products and accumulate in output array
-    let rightProduct = 1;
-    for (let i = n - 1; i >= 0; i--) {
-        output[i] *= rightProduct; // Equivalent to: output[i] = output[i] * rightProduct;
-        rightProduct *= nums[i];   // Update rightProduct for the next iteration
-    }
-    
-    return output;
+    isPalindrome(s) {
+s=s.toLowerCase()
+function isAlphaNumeric(c){
+   return (c >= 'A' && c <= 'Z') || 
+               (c >= 'a' && c <= 'z') || 
+               (c >= '0' && c <= '9');
 }
+let cleanString='';
+for(let i=0;i<s.length;i++){
+    if(isAlphaNumeric(s[i])){
+         cleanString += s[i];
+    }
+}
+let reverseString=cleanString.split('').reverse().join('')
+return (cleanString===reverseString)
 
 
-
+    }
 }
