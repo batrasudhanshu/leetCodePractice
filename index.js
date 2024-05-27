4b9c751155e6072118b67b66612a5f5b0b381caa
sudhanshu1998 <sudhanshu.batra@solulever.com>
2024-05-27 11:32:06 +0530

    solution submitted

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 class Solution {
     /**
-     * @param {string} s
-     * @return {boolean}
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
      */
-    isPalindrome(s) {
-s=s.toLowerCase()
-function isAlphaNumeric(c){
-   return (c >= 'A' && c <= 'Z') || 
-               (c >= 'a' && c <= 'z') || 
-               (c >= '0' && c <= '9');
-}
-let cleanString='';
-for(let i=0;i<s.length;i++){
-    if(isAlphaNumeric(s[i])){
-         cleanString += s[i];
+    twoSum(numbers, target) {
+        let left=0;
+        let right=numbers.length-1;
+
+         while (left < right) {
+        const sum = numbers[left] + numbers[right];
+        
+        if (sum === target) {
+            return [left + 1, right + 1]; // Return 1-indexed positions
+        } else if (sum < target) {
+            left++; // Move left pointer to the right to increase the sum
+        } else {
+            right--; // Move right pointer to the left to decrease the sum
+        }
     }
-}
-let reverseString=cleanString.split('').reverse().join('')
-return (cleanString===reverseString)
 
-
+    return [];
     }
 }
